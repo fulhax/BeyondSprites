5a75b0218c02491806739b27f7df3d107e61123a
c0r73x <c0r73x@fulhax.nu>
2013-04-23 20:55:37 +0200

    Updated Makefile to work with glfw

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 
 int main(int argc, char const* argv[])
 {
-    printf("foobar\n");
+    glfwInit();
+    if(glfwOpenWindow(640,480,5,6,5,0,8,0,GLFW_WINDOW) != GL_TRUE)
+    {
+        printf("Pooped");
+        return -1;
+    }
+
     return 0;
 }
