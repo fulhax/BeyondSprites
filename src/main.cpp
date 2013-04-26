781bd09c6cf27b6577d7045641a6403026853306
c0r73x <c0r73x@fulhax.nu>
2013-04-26 17:30:21 +0200

    Added assimp code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
 #include <stdio.h>
 #include <GL/glfw.h>
 
+#include <assimp/cimport.h>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+bool Import(const char *file)
+{
+    const aiScene* s = aiImportFile(file, aiProcess_CalcTangentSpace | aiProcess_Triangulate | aiProcess_JoinIdenticalVertices | aiProcess_SortByPType);
+
+    if(!s)
+    {
+        printf("ohnos! %s\n", aiGetErrorString());
+        return false;
+    }
+
+    printf("Loaded %s, and all is goooooood\n", file);
+
+    aiReleaseImport(s);
+    return true;
+}
+
 int main(int argc, char const* argv[])
 {
-    glfwInit();
-    if(glfwOpenWindow(640,480,5,6,5,0,8,0,GLFW_WINDOW) != GL_TRUE)
-    {
-        printf("Pooped");
-        return -1;
-    }
+    Import("./player/player_corpse.md5mesh");
+
+    /*
+     *glfwInit();
+     *if(glfwOpenWindow(640,480,5,6,5,0,8,0,GLFW_WINDOW) != GL_TRUE)
+     *{
+     *    printf("Pooped");
+     *    return -1;
+     *}
+     */
 
     return 0;
 }
